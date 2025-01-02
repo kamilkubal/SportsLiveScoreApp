c9342748a54bd819882e228c1172dcae967bb135
Kamil Kubal <k.kubal@ideo.pl>
2025-01-02 11:55:41 +0100

    Add domain layer to project

--- SportsLiveScoreApp.Application/Helpers/ModelHelpers.cs
+++ SportsLiveScoreApp.Application/Helpers/ModelHelpers.cs
@@ -1,4 +1,4 @@
-﻿using SportsLiveScoreApp.Application.Helpers.Enums;
+﻿using SportsLiveScoreApp.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- SportsLiveScoreApp.Domain/Enums/MatchEventType.cs
+++ SportsLiveScoreApp.Domain/Enums/MatchEventType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SportsLiveScoreApp.Application.Helpers.Enums
+namespace SportsLiveScoreApp.Domain.Enums
 {
     public enum MatchEventType
     {
